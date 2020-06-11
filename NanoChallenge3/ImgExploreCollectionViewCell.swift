5b1a2ce69b07bc99ed3c948522056f2d74aa12ae
intan318 <34388370+intan318@users.noreply.github.com>
2020-06-11 14:37:45 +0700

    first commit to tabbar

---
+++ NanoChallenge3/ImgExploreCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImgExploreCollectionViewCell.swift
+//  NanoChallenge3
+//
+//  Created by Intan Yoshana on 11/06/20.
+//  Copyright © 2020 Intan Yoshana. All rights reserved.
+//
+
+import UIKit
+
+class ImgExploreCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgCell: UIImageView!
+}
