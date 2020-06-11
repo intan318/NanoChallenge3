20f4fbd1ae966d2ffaa4b3766629cb183f088565
rafikaalifia <62131104+rafikaalifia@users.noreply.github.com>
2020-06-11 15:32:06 +0700

    change scrollview
    add slide.swit and .xib

---
+++ NanoChallenge3/Slide.swift
@@ -0,0 +1,24 @@
+//
+//  Slide.swift
+//  NanoChallenge3
+//
+//  Created by Rafika Alifia Isti on 11/06/20.
+//  Copyright © 2020 Intan Yoshana. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var shortBriefLabel: UILabel!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- NanoChallenge3/ViewController.swift
+++ NanoChallenge3/ViewController.swift
@@ -12,26 +12,51 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     
-    var imageArray = [UIImage]()
+    var slides:[Slide] = [];
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        imageArray = [#imageLiteral(resourceName: "02m"),#imageLiteral(resourceName: "02n"),#imageLiteral(resourceName: "02o")]
- 
-        for i in 0..<imageArray.count {
-            
-            let imageView = UIImageView()
-            imageView.image = imageArray[i]
-            let xPosition = self.view.frame.width * CGFloat(i)
-            imageView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height:  self.scrollView.frame.height)
-            
-            scrollView.contentSize.width = scrollView.frame.width * CGFloat(i+1)
-            scrollView.addSubview(imageView)
-        }
-        
+      slides = createSlide()
+      setupSlideScrollView(slides: slides)
+      
+      pageControl.numberOfPages = slides.count
+      pageControl.currentPage = 0
+      view.bringSubviewToFront(pageControl)
+
     }
     
+    //load slide content to the scrollview
+    func createSlide() -> [Slide] {
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide1.imageView.image = UIImage(named: "02m")
+        slide1.titleLabel.text = "Lawu Mountain"
+        slide1.shortBriefLabel.text = "is located on the border of Central Java and East Java. This Mountain is a volcano that has long been inactive with the highest point 3265 m above sea level"
+        
+        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide2.imageView.image = UIImage(named: "02n")
+        slide2.titleLabel.text = "Everest Mountain"
+        slide2.shortBriefLabel.text = "is located on the border of Central Java and East Java. This Mountain is a volcano that has long been inactive with the highest point 3265 m above sea level"
+        
+        let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide3.imageView.image = UIImage(named: "02o")
+        slide3.titleLabel.text = "Beach"
+        slide3.shortBriefLabel.text = "is located on the border of Central Java and East Java. This Mountain is a volcano that has long been inactive with the highest point 3265 m above sea level"
+        
+        return[slide1, slide2, slide3]
+    }
+    
+    //initializing a list of 3 slides to be added as a subview to the scrollview
+    func setupSlideScrollView(slides : [Slide]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
 
     
 
