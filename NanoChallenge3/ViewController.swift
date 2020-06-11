530a9bee3f764840833e6f48945a907539019b24
rafikaalifia <62131104+rafikaalifia@users.noreply.github.com>
2020-06-11 12:53:50 +0700

    add image scrollview
    add image scrollview

--- NanoChallenge3/ViewController.swift
+++ NanoChallenge3/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var imageArray = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        imageArray = [#imageLiteral(resourceName: "02m"),#imageLiteral(resourceName: "02n"),#imageLiteral(resourceName: "02o")]
+ 
+        for i in 0..<imageArray.count {
+            
+            let imageView = UIImageView()
+            imageView.image = imageArray[i]
+            let xPosition = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height:  self.scrollView.frame.height)
+            
+            scrollView.contentSize.width = scrollView.frame.width * CGFloat(i+1)
+            scrollView.addSubview(imageView)
+        }
+        
     }
+    
 
+    
 
 }
 
